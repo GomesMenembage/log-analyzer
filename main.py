4f3360c0fe4329ffc7a53f7c251773ad4324fa0d
GomesMenembage <mikeykkj15@gmail.com>
2026-06-17 14:07:09 +0100

    refator: file path verification

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 def read_log_file(path):
+
+    if path.endswith("/"):
+        print("Path is a folder, try with a file")
     try:
         with open (path,"r", encoding="utf-8") as log_file:
             for line in log_file:
-                print(line)
+                valid_line = line.strip()
+                if valid_line:
+                    yield line
+
 
     except FileNotFoundError:
         print("File was not founded")
+
+for line in read_log_file("/home/dm/log-analyzer/README.md"):
+     print(line)
